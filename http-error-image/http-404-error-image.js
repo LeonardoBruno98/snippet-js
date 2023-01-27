6ecee2a7729fc95d9e6500b1539016b3632ccc42
Leonardo Bruno <46956703+LeonardoBruno98@users.noreply.github.com>
2023-01-27 15:20:08 +0100

    Delete http-error-image directory

--- http-error-image/http-404-error-image.js
+++
@@ -1,7 +0,0 @@
-$( document ).ready(function() {
-	$( "img" ).on( "error", function() {
-		var $img = $( this );
-		var placeholder = location.protocol + "//" + location.host + "/images/placeholder.png";
-		$img.attr( "src", placeholder );
-	});
-});
